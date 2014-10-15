99c97decaf64c662556793d5a046b7f3a3fa3bb3
Haris Mahmood <harismahmood89@gmail.com>
2014-10-14 20:44:45 -0400

    bugfix for comments routing

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
 	# validates_presence_of :title, :body
 	# validates_uniqueness_of :title
 
-	has_many :comments
+	has_many :comments, dependent: :destroy
 
 	validates :title, presence: true, length: {minimum: 5}
 	validates :body, presence: true
