f29ce175b8104afbc248cb898db148d3b9d5531d
Haris Mahmood <harismahmood89@gmail.com>
2014-11-07 00:55:49 -0500

    style cleanup, social icons moved, overlay work

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,14 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+  initOverlay();
+});
+
+function initOverlay(){
+  $('.js-toggleOverlay').on('click', function(e){
+      e.preventDefault();
+      $('.overlay').toggleClass('active');
+  });
+}
