c4fbc0296ce1f9e312b40c0f3abfaecc20e1ca7c
Haris Mahmood <harismahmood89@gmail.com>
2014-11-09 23:27:46 -0500

    style cleanup, admin routes, admin styles, favicon

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,13 @@ Rails.application.routes.draw do
 
   root "posts#index"
 
+  devise_scope :user do
+    get "/login" => "devise/sessions#new"
+    delete "/logout" => "devise/sessions#destroy"
+  end
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
